252145b6bb3c6d5bfd4ed4a0df549ebdee89f3cd
matte-alt <dsksnkz83@gmail.com>
2026-06-03 18:39:44 +0100

    added more apps

--- install.bash
+++ install.bash
@@ -6,6 +6,6 @@ cd .config
 
 git clone https://github.com/dsksnkz/my-hyprland-rice.git
 
-pacman -S cava rofi waybar fastfetch steam wlogout firefox nmtui helium
+pacman -S cava rofi waybar fastfetch steam wlogout firefox nmtui helium obsidian blender logseq fish
 
 exit
