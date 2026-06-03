b1b8896486deb7c293c1b264dbbf3b324010a597
matte <duskbladez83@gmail.com>
2026-06-03 21:12:16 +0100

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,2 +1,2 @@
 # For NORMAL arch-hyprland dots
-sudo cd .config && git clone https://github.com/dsksnkz/ifunx-dotfiles.git && pacman -S cava rofi waybar fastfetch steam wlogout firefox nmtui helium obsidian blender logseq fish hyprlock hyprland-plugin-csgo-vulkan-fix && echo Hyprland-Default - Installation either completed or failed. You may now close this Window or report a bug. 
+sudo cd .config && git clone https://github.com/dsksnkz/Matte_Hyprland.git && pacman -S cava rofi waybar fastfetch steam wlogout firefox nmtui helium obsidian blender logseq fish hyprlock hyprland-plugin-csgo-vulkan-fix && echo Hyprland-Default - Installation either completed or failed. You may now close this Window or report a bug. 
