c3fc707e4f7dae243d126b6350a902e28b969207
Ash <dsksnkz83@gmail.com>
2026-06-03 19:13:30 +0100

    Create installNIRIWM.bash

---
+++ installNIRIWM.bash
@@ -0,0 +1,9 @@
+# For installing NIRI WM external (others are still normal)
+sudo
+cd .config
+
+git clone https://github.com/dsksnkz/my-hyprland-rice.git
+
+pacman -S niri-git cava rofi waybar fastfetch steam wlogout firefox nmtui helium obsidian blender logseq fish hyprlock hyprland-plugin-csgo-vulkan-fix 
+
+exit
